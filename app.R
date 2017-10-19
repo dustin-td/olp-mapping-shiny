f42ce9e69a5f7ab0d6ab8c882388989341fe3975
stuartmason <strtmason@gmail.com>
2017-10-19 11:37:16 -0400

    single function draws many maps

--- app.R
+++ app.R
@@ -3,39 +3,29 @@ library('rgdal') # This library allows us to read and plot shapefiles
 library('leaflet') # Interactivity! 
 library('data.table')
 
-moz.p <- readOGR(dsn="data/MOZ_adm_shp", "MOZ_adm3")
-moz.p$rn <- row.names(moz.p)
-#data <- geojson_json(moz.p)
-moz.data <- as.data.table(moz.p@data)
-csv.data <- as.data.table(read.csv("data/Maps_9-1.csv", header=T))
+source('R/data_load.R')
+source('R/map_classes.R')
+source('R/map_class_utilities.R')
 
-csv.data <- csv.data[, .(gadmPA, zScore, zpor, zelo, zemk, zech)]
-setnames(csv.data, "gadmPA", "NAME_3")
+moz.p <- MapAndChloroData()
+match.data <- as.data.table(read.csv("gis/data/Match.csv", header=T, fileEncoding="utf-8"))
 
-setkey(csv.data, NAME_3)
-setkey(moz.data, NAME_3)
+map.meta.data <- ConstructMapDataObjects(chloro.raw, moz.p, match.data)
+domain <- GetFillDomain(map.meta.data)
 
-moz.data <- csv.data[moz.data]
-
-setkey(moz.data, rn)
-moz.p@data <- moz.data[row.names(moz.p)]
-
-languages <- c('Overall', 'Portuguese', 'Elomwe', 'Emakhua', 'Echuwabo')
-
-# DRY way of segmenting the map
-colnames <- c('zScore', 'zpor', 'zelo', 'zemk', 'zech')
-for(colname in colnames) {
-  
-  sel <- moz.p@data[, !is.na(get(colname))]
-  assign(paste0('moz.', colname), moz.p[sel, ])
-  
-}
+map.options <- names(map.meta.data)
 
 # A dumb palette we will probably want to change
 pal <- colorNumeric(
   palette = "Reds",
-  domain = unique(c(csv.data$zpor, csv.data$zelo, csv.data$zemk, csv.data$zech, csv.data$zScore))
+  domain = domain
 )
+
+matchpal <- colorFactor(
+  palette = "Reds",
+  domain = levels(match.data$MatchCat)
+)
+
 
 ui <- shinyUI(fluidPage(
   titlePanel(
@@ -46,7 +36,7 @@ ui <- shinyUI(fluidPage(
          </header>"
     )),
   selectInput("language", "Language",
-              languages
+              map.options
   ),
   mainPanel(
     leafletOutput("moz.map")
@@ -63,13 +53,9 @@ server <- shinyServer(function(input, output) {
     # Add static polygons, the entire map without any special fill
     leaf <- addPolygons(leaf, data=moz.p, weight=1, group="static", color = "#000000", fillOpacity=0.4)
     
-    # Add all polygons and hide them (We'll want to DRY this up as well)
-    leaf <- addPolygons(leaf, data=moz.zScore, weight=1, group='Overall', color = "#000000", fillColor=~pal(zScore), fillOpacity=0.4)
-    leaf <- addPolygons(leaf, data=moz.zpor, weight=1, group='Portuguese', color = "#000000", fillColor=~pal(zpor), fillOpacity=0.4)
-    leaf <- addPolygons(leaf, data=moz.zelo, weight=1, group='Elomwe', color = "#000000", fillColor=~pal(zelo), fillOpacity=0.4)
-    leaf <- addPolygons(leaf, data=moz.zemk, weight=1, group='Emakhua', color = "#000000", fillColor=~pal(zemk), fillOpacity=0.4)
-    leaf <- addPolygons(leaf, data=moz.zech, weight=1, group='Echuwabo', color = "#000000", fillColor=~pal(zech), fillOpacity=0.4)
-    hideGroup(leaf, languages)
+    # Add all polygons and hide them 
+    for(language in map.options) leaf <- DrawMap(map.meta.data[[language]], leaf)
+    hideGroup(leaf, map.options)
     
   })
   
@@ -78,7 +64,7 @@ server <- shinyServer(function(input, output) {
     new.leaf <- leafletProxy('moz.map')
     
     # Defensively hide all groups
-    hideGroup(new.leaf, languages)
+    hideGroup(new.leaf, map.options)
     # Show selected groups
     showGroup(new.leaf, choice)
   })
